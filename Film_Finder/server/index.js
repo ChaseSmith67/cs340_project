85ccc130ed68c8c5936516442428106640523ce7
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-02-28 22:03:54 -0600

    setup db connection

--- Film_Finder/server/index.js
+++ Film_Finder/server/index.js
@@ -1,8 +1,14 @@
 import express from "express"
 
+// Express Setup
 const app = express();
 const PORT = 4989;
 
+// Database
+const db = require('./db-connector');
+
+
+// Listener
 app.listen(PORT, () => { 
     console.log("Running on port " + PORT) 
 });
